16124dae2e70ef36aebb31463e1fa877518bb805
Jeffrey Platt <jsplatt@gmail.com>
2021-07-01 17:15:55 -0400

    spotify hooked up to react

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login'
 import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js'
+
+const spotify = new SpotifyWebApi()
 
 function App() {
+  const [token, setToken] = useState(null)
+
   useEffect(() => {
-    const token = getTokenFromUrl()    
+    const hash = getTokenFromUrl()   
+    window.location.hash=""
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token)
+
+      spotify.setAccessToken(_token)
+
+      spotify.getMe().then(user => {
+        console.log('💆' , user)
+      })
+    }
+
+    console.log("I HAVE A TOKEN>>>>", token)
   }, [])
 
   return (
     <div className="app">
-      <Login />
+      {
+        token ? (
+          <h1>I am logged in</h1>
+      ) : (
+         <Login />
+      )
+    }
     </div>
   );
 }
