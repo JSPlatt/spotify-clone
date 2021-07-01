5bb6efc101aa1fc427a94bea995dec4db51ce951
Jeffrey Platt <jsplatt@gmail.com>
2021-07-01 16:49:35 -0400

    access url token

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useEffect } from 'react';
 import './App.css';
 import Login from './Login'
+import { getTokenFromUrl } from './spotify';
 
 function App() {
+  useEffect(() => {
+    const token = getTokenFromUrl()    
+  }, [])
+
   return (
     <div className="app">
       <Login />
