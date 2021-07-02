992e270e9f9c72ce6ec7c16ddf85553a2a00d20d
Jeffrey Platt <jsplatt@gmail.com>
2021-07-02 09:49:01 -0400

    player page started

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Sidebar.css'
 
 function Sidebar() {
     return (
-        <div>
-            
+        <div className="sidebar">
+            <h1>I'm a SIDEBAR</h1>
         </div>
     )
 }
