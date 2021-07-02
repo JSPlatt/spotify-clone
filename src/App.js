b18f8f51bb2da8e1f20753ef8e856e553f970bd2
Jeffrey Platt <jsplatt@gmail.com>
2021-07-01 23:03:14 -0400

    able to pull from data layer

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import Login from './Login'
 import { getTokenFromUrl } from './spotify';
 import SpotifyWebApi from 'spotify-web-api-js'
 import Player from './Player'
+import { useDataLayerValue } from './DataLayer';
 
 const spotify = new SpotifyWebApi()
 
 function App() {
   const [token, setToken] = useState(null)
+  const [{ user }, dispatch] = useDataLayerValue()
 
   useEffect(() => {
     const hash = getTokenFromUrl()   
@@ -22,6 +24,10 @@ function App() {
 
       spotify.getMe().then(user => {
     
+        dispatch({
+          type: 'SET_USER',
+          user: user
+        })
       })
     }
   }, [])
